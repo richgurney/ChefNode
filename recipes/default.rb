4466dbaa57d39b33d0d6f125bfbf6515d104676b
Richard Gurney <rgurney@spartaglobal.com>
2018-09-25 12:14:33 +0100

    adding cloud yaml

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,37 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+apt_update "update_sources" do
+  action :update
+end
+
+package "nginx" do
+  action :install
+end
+
+service "nginx" do
+  action [:enable, :start]
+end
+
+template '/etc/nginx/sites-available/proxy.conf' do
+  source 'proxy.conf.erb'
+  notifies(:restart, 'service[nginx]')
+end
+
+link '/etc/nginx/sites-enabled/proxy.conf' do
+  to '/etc/nginx/sites-available/proxy.conf'
+  notifies(:restart, 'service[nginx]')
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+  notifies(:restart, 'service[nginx]')
+end
+
+include_recipe("nodejs")
+
+nodejs_npm("pm2") do
+  action :install
+end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,17 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+# This is an example test, replace it with your own test.
+
+describe service("nginx") do
+  it { should be_running }
+  it { should be_enabled }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(80) do
+  it { should be_listening }
+end
+
+describe http("http://localhost", enable_remote_worker: true) do
+  its('status') { should cmp 502}
 end
